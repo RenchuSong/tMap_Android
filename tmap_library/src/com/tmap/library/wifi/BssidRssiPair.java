2cc8a70978c9ac68a5135fb65f0b4e445139955f
RenchuSong <songrenchu@sina.com>
2014-04-20 16:20:54 +0800

    wifi scan

---
+++ tmap_library/src/com/tmap/library/wifi/BssidRssiPair.java
@@ -0,0 +1,11 @@
+package com.tmap.library.wifi;
+
+public class BssidRssiPair {
+	public String bssid;
+	public int rssi;
+	
+	public BssidRssiPair(String bssid, int rssi) {
+		this.bssid = bssid;
+		this.rssi = rssi;
+	}
+}
