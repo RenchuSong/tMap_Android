6fcd4812b9b741c7d4132f25bcc86d891c415796
RenchuSong <songrenchu@sina.com>
2014-05-20 22:12:13 -0700

    log change

--- tmap_model_tool/src/com/tmap/model/tool/arch/AppConfig.java
+++ tmap_model_tool/src/com/tmap/model/tool/arch/AppConfig.java
@@ -3,7 +3,7 @@ package com.tmap.model.tool.arch;
 import com.tmap.library.SystemConfig;
 
 public class AppConfig extends SystemConfig{
-	public static String SERVER_ADDR = "http://10.206.8.74/";	
-	//public static String SERVER_ADDR = "http://64.9.235.60/";	
+	//public static String SERVER_ADDR = "http://10.206.8.74/";	
+	public static String SERVER_ADDR = "http://192.168.43.133/";	
 	
 }
